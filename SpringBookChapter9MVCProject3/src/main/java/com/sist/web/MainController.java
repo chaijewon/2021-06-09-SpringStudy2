5229f8d2f5021259479c3a5a4a7b738d15853550
chaijewon <vcandjava@nate.com>
2021-10-08 08:26:53 +0900

    111

---
+++ SpringBookChapter9MVCProject3/src/main/java/com/sist/web/MainController.java
@@ -0,0 +1,27 @@
+package com.sist.web;
+
+import org.springframework.stereotype.Controller;
+
+// Model => 
+@Controller 
+/*
+ *    프로젝트 생성 ==> 1. 버전 변경 
+ *                  2. pom.xml 
+ *                  3. web.xml 
+ *                  4. application-context.xml
+ *                  5. 자바 
+ *                     === 
+ *                     1) Controller 
+ *                     2) DAO 
+ *                        = VO
+ *                        = DAO
+ *                        = Mapper
+ *    요청 받아서 => 처리 => JSP에 값을 전송 
+ *    
+ *    1. request를 받아서 처리 
+ *    2. DispatcherServlet로 부터 요청값을 받아서 처리 
+ *       =============
+ */
+public class MainController {
+
+}
